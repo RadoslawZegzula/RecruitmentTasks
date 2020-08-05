1580d71c0e1bd9b3a9fc4ec1c20c1d3a54f8f1d5
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-06 01:52:34 +0200

    2-add-models-and-database

---
+++ BackendPython/programlogic/location.py
@@ -0,0 +1,17 @@
+from peewee import *
+db = SqliteDatabase('programlogic/personDatabase.db')
+
+class Location(Model):
+        streetNumber = IntegerField(null=True)
+        streetName = CharField(max_length=70, null=True)
+        city = CharField(max_length=70, null=True)
+        state = CharField(max_length=70, null=True)
+        country = CharField(max_length=70, null=True)
+        postcode = IntegerField(null=True)
+        latiduteCordinates = CharField(max_length=70, null=True)
+        longitudeCordinates = CharField(max_length=70, null=True)
+        offsetTimezone = CharField(max_length=70, null=True)
+        descriptionTimezone = CharField(max_length=70, null=True)
+      
+        class Meta:
+                database=db
