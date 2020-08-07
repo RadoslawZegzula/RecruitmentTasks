279b871073d27d977d094df84260286daa6ad4ed
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-07 16:23:48 +0200

    4- Add argparse user interface file entry

--- BackendPython/guiq.py
+++ BackendPython/guiq.py
@@ -1,7 +1,10 @@
 import argparse
+from searcherByUserCommand import SearcherByUserCommand 
 
 parser = argparse.ArgumentParser(description = 'Gui to query person database')
 parser.add_argument("category", help = "Category of query")
 parser.add_argument("-f", "--firstOptional", help = "First optional argument")
 parser.add_argument("-s", "--secondOptional", help = "Second optional argument")
 args = parser.parse_args()
+
+sbuc = SearcherByUserCommand(args)
